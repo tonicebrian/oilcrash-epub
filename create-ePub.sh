301299bb146d792438ecaa5a28b30102e69025a1
Toni Cebrián <ancechu@gmail.com>
2012-12-13 21:57:17 +0100

    Cleaning directories

--- create-ePub.sh
+++ create-ePub.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+mkdir -p target
+cd target
 #wget http://crashoil.blogspot.com/feeds/posts/default?max-results=300 -O oilcrash.xml
-#wget http://crashoil.blogspot.com/feeds/posts/default -O oilcrash.xml
-xmllint --noent --format oilcrash.xml > oilcrash-pretty.xml
-xsltproc atom-to-html.xsl src/test/scala/oilcrash-pretty.xml > oilcrash.xhtml
+#wget http://crashoil.blogspot.com/feeds/posts/default -O oilcrash-raw.xml
+xmllint --noent --format oilcrash-raw.xml > oilcrash-pretty.xml
+xsltproc ../atom-to-html.xsl oilcrash-pretty.xml > oilcrash.xhtml
 perl -pi -e "s/&lt;/</g" oilcrash.xhtml
 perl -pi -e "s/&gt;/>/g" oilcrash.xhtml
 perl -pi -e "s/&amp;/&/g" oilcrash.xhtml
@@ -12,4 +14,7 @@ perl -pi -e "s/<\/span>//g" oilcrash.xhtml
 perl -pi -e "s/<span[^>]*>//g" oilcrash.xhtml
 tidy -utf8 -asxhtml -m oilcrash.xhtml
 ebook-convert oilcrash.xhtml oilcrash.epub
+
+cp oilcrash.epub ..
+cd -
 
