2d9114d8fe9f870f842d852f8277ce9ef00f5af5
Toni Cebrián <ancechu@gmail.com>
2012-12-14 13:53:51 +0100

    Final version of the scripts

--- create-ePub.sh
+++ create-ePub.sh
@@ -6,19 +6,22 @@ mkdir -p target
 cd target
 #wget http://crashoil.blogspot.com/feeds/posts/default?max-results=300 -O oilcrash.xml
 #wget http://crashoil.blogspot.com/feeds/posts/default -O oilcrash-raw.xml
-xsltproc ../atom-to-html.xsl oilcrash-raw.xml > oilcrash.xhtml
-perl -pi -e "s/&lt;/</g" oilcrash.xhtml
-perl -pi -e "s/&gt;/>/g" oilcrash.xhtml
-perl -pi -e "s/&amp;/&/g" oilcrash.xhtml
-perl -pi -e "s/&nbsp;/ /g" oilcrash.xhtml
-perl -pi -e "s/<\/span>//g" oilcrash.xhtml
-perl -pi -e "s/<span[^>]*>//g" oilcrash.xhtml
-perl -pi -e 's/http:\/\/crashoil.blogspot.com.es\/(\d+)\/(\d+)\/([^.]+).htm[l]?/#\1-\2-\3/g' oilcrash.xhtml
-tidy -i -utf8 -asxhtml -m oilcrash.xhtml
+#xsltproc ../atom-to-html.xsl oilcrash-raw.xml > oilcrash.xhtml
+#perl -pi -e "s/&lt;/</g" oilcrash.xhtml
+#perl -pi -e "s/&gt;/>/g" oilcrash.xhtml
+#perl -pi -e "s/&amp;/&/g" oilcrash.xhtml
+#perl -pi -e "s/&nbsp;/ /g" oilcrash.xhtml
+#perl -pi -e "s/<\/span>//g" oilcrash.xhtml
+#perl -pi -e "s/<span[^>]*>//g" oilcrash.xhtml
+#perl -pi -e 's/http:\/\/crashoil.blogspot.com.es\/(\d+)\/(\d+)\/([^.]+).htm[l]?/#\1-\2-\3/g' oilcrash.xhtml
+#tidy -i -utf8 -asxhtml -m oilcrash.xhtml
+#
+#python ../image_downloader.py oilcrash.xhtml 
 
-python ../image_downloader.py oilcrash.xhtml 
-
-ebook-convert oilcrash-final.xhtml oilcrash.epub --authors "Antonio Turiel"
+# Componer el eBook
+wget http://altermon.files.wordpress.com/2012/11/crude1.jpg
+cp ../Portada.xhtml .
+ebook-convert oilcrash-final.xhtml oilcrash.epub --authors "Antonio Turiel" --language es --title "The Oil Crash blog" --cover crude1.jpg
 
 cp oilcrash.epub ..
 cd -
